5c133e052a29e0c7863c993feaa9c4a761b0e879
Piotr Podstawski <piotr.podstawski@gammanet.pl>
2015-05-12 11:27:18 +0200

    debug

--- application/widgets/merlin/include/pre.php
+++ application/widgets/merlin/include/pre.php
@@ -5,9 +5,9 @@
     if (!isset($merlin) || !is_object($merlin))
     {
         require_once __DIR__.'/Merlin.php';
-        parse_str($costxt);
         $merlin=new MERLIN($login,$pass,$operator);
     }
+    parse_str($costxt);
     
     
     $merlin_q=array('dest'=>'','dep'=>'');
