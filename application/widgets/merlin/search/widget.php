17dabbe3fd20a752b2531f819513ad9d28f7457e
Piotr Podstawski <piotr.podstawski@gammanet.pl>
2015-05-22 11:11:35 +0200

    datepicker

--- application/widgets/merlin/search/widget.php
+++ application/widgets/merlin/search/widget.php
@@ -15,9 +15,20 @@ class merlin_searchWidget extends merlinWidget
 
     public function run()
     {
-        $this->loadJS('../bootstrap-datepicker.js');
-	$this->loadCSS('../datepicker.css');
+        //$this->loadJS('../bootstrap-datepicker.js');
+	//$this->loadCSS('../datepicker.css');
         
+	$this->loadJS('../picker.js');
+	$this->loadJS('../picker.date.js');
+	$this->loadJS('../picker.time.js');
+	$this->loadJS('../legacy.js');
+	$this->loadJS('../translations/pl_PL.js');
+	
+	$this->loadCSS('../themes/default.css');
+	$this->loadCSS('../themes/default.date.css');
+	
+	
+	
         $merlin=$this->merlin;
         $merlin_searchWidget=array();
 	$costxt=$this->webtd['costxt'];
