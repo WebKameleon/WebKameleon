4c39eb5a5aad16d92d557555c890fb58a94e358d
Piotr Podstawski <piotr@webkameleon.com>
2016-05-10 10:04:17 +0000

    slider

--- application/widgets/slideshow/images/slideshow.js
+++ application/widgets/slideshow/images/slideshow.js
@@ -1,8 +1,10 @@
 jQueryKam(function ($) {
     if (typeof(kmw_slideshowArray)!='undefined') {
+        $('.bxslider-wrapper').show();
+        
         for (i=0;i<kmw_slideshowArray.length;i++) {
             $("#bxslider"+kmw_slideshowArray[i].sid).bxSlider(kmw_slideshowArray[i].opt);
         }
-        $('.bxslider-wrapper').show();
+        
     }
 });
